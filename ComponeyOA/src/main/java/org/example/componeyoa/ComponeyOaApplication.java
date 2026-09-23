815c9e6d2a76924abe51fc0be2089f8bbd5fb4bc
a222641856164-2188 <a222641856164@gmail.com>
2026-09-23 23:16:34 +0800

    前后端搭建。

---
+++ ComponeyOA/src/main/java/org/example/componeyoa/ComponeyOaApplication.java
@@ -0,0 +1,13 @@
+package org.example.componeyoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponeyOaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponeyOaApplication.class, args);
+    }
+
+}
---
+++ ComponeyOA/src/test/java/org/example/componeyoa/ComponeyOaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.componeyoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponeyOaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
